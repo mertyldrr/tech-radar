81f062c82be9bf898cdf3d09a53bed66839d4afa
mertyldrr <mertyildirir14@gmail.com>
2023-06-16 20:52:56 +0200

    add enums

---
+++ src/enums.ts
@@ -0,0 +1,14 @@
+export enum ComboBoxVariant {
+  Quadrant = "quadrant",
+  Ring = "ring",
+  Moved = "moved",
+  Active = "active",
+}
+
+export enum ErrorField {
+  TechName = "techName",
+  QuadrantValue = "quadrantValue",
+  RingValue = "ringValue",
+  MovedValue = "movedValue",
+  ActiveValue = "activeValue",
+}
